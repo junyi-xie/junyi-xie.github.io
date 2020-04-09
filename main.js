4e91db2256ab3a192cbcb40196d7a1cf6d658e43
Yunyi1 <yunyi.xie@outlook.com>
2020-04-09 23:07:19 +0200

    Added title and changed hover color.

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function scrollToProject() {
 }
 
 // Typewriting javascript effect. CREDIT TO https://stackoverflow.com/users/2417833/scraaappy
-var messages=["I am Jun Yi Xie.","I am a Student.","I am a Front-End Developer."];
-var rank=0;
+var messages = ["I am Junyi Xie.","I am a Student.","I am a Front-End Developer."];
+var rank = 0;
 
 // Code for Chrome, Safari and Opera
 document.getElementById("typewriter").addEventListener("webkitAnimationEnd", changeTxt);
@@ -18,7 +18,7 @@ function changeTxt(e){
   h1 = this.getElementsByTagName("h1")[0];
   h1.style.webkitAnimation = 'none'; // set element animation to none
    setTimeout(function() { // you surely want a delay before the next message appears
-      h1.innerHTML= messages[rank];
+      h1.innerHTML = messages[rank];
       var speed = 2.5*messages[rank].length/20; // adjust the speed (3.5 is the original speed, 20 is the original string length
       h1.style.webkitAnimation = 'typing '+speed+'s steps(20, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
       (rank===messages.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
